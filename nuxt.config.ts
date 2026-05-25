5544e1c39805bb87f442f017518cd1a6c1a96983
NetELECdev <naferlau@gmail.com>
2026-05-25 13:21:26 -0300

    Configure Nuxt for Netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,14 @@ export default defineNuxtConfig({
   devServer: {
     port: 8080
   },
+
   app: {
     head: {
       title: 'Control Iluminación con ESP8266'
     }
+  },
+
+  nitro: {
+    preset: "netlify"
   }
 })
